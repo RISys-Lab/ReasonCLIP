a111e9ace870dc9a9e0984f410e3deb8ef9b5fc2
fesvhtr <fesvhtr@gmail.com>
2025-06-24 00:34:40 +0400

    fix

--- scripts/gen_vllm_ray_visual.sh
+++ scripts/gen_vllm_ray_visual.sh
@@ -17,14 +17,14 @@ python -u dataset/gen_vllm_ray_visual.py \
     --parquet_dir_path $WORK/fmohamma/CLIP-R/data/cc12m-anno/cc12m_chunk_00.parquet \
     --image_dir_path $WORK/fmohamma/CLIP-R/data/cc12m/ \
     --checkpoint_interval 500000 \
-    --batch_size 48 \
+    --batch_size 8 \
     --max_model_len 2048 \
-    --max_num_batched_tokens 65536 \
-    --max_num_seqs 48 \
+    --max_num_batched_tokens 16384 \
+    --max_num_seqs 8 \
     --max_tokens 1024 \
     --temperature 0.8 \
     --top_p 0.95 \
-    --tensor_parallel_size 4 \
+    --tensor_parallel_size 2 \
     --pipeline_parallel_size 1 \
     --gpu_memory_utilization 0.9 \
     --enable_chunked_prefill \
