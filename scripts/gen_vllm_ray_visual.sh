689cef9c5b84958c7877ba6936924fa8cd294ad2
fesvhtr <fesvhtr@gmail.com>
2025-07-08 19:01:48 +0400

    update

--- scripts/gen_vllm_ray_visual.sh
+++ scripts/gen_vllm_ray_visual.sh
@@ -14,14 +14,14 @@ export TOKENIZERS_PARALLELISM=false
 python -u dataset/gen_vllm_ray_visual.py \
     --model_source $WORK/fmohamma/CLIP-R/data/Qwen2.5-VL-72B-Instruct-AWQ \
     --output_dir_path  $WORK/fmohamma/CLIP-R/outputs/ReasonLite/cc12m_tb \
-    --parquet_dir_path $WORK/fmohamma/CLIP-R/data/cc12m-anno/cc12m_chunk_02.parquet \
+    --parquet_dir_path $WORK/fmohamma/CLIP-R/data/cc12m-anno/cc12m_chunk_00.parquet \
     --image_dir_path $WORK/fmohamma/CLIP-R/data/cc12m/ \
-    --checkpoint_interval 80000 \
-    --ray_batch_size 80000 \
-    --batch_size 40 \
+    --checkpoint_interval 100000 \
+    --ray_batch_size 100000 \
+    --batch_size 50 \
     --max_model_len 2048 \
-    --max_num_batched_tokens 65536 \
-    --max_num_seqs 40 \
+    --max_num_batched_tokens 81920 \
+    --max_num_seqs 50 \
     --max_tokens 1024 \
     --temperature 0.8 \
     --top_p 0.95 \
