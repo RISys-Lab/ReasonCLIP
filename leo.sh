7a30885ea1afa5b50ba503b8bfe9ebbab461bd20
fesvhtr <fesvhtr@gmail.com>
2025-07-11 00:50:15 +0400

    update

--- leo.sh
+++ leo.sh
@@ -2,15 +2,15 @@
 #SBATCH --job-name=gen_cc12m_01
 #SBATCH --time=4-00:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH --partition=boost_usr_prod
 #SBATCH --qos=boost_qos_lprod
 #SBATCH --output=gen_cc12m_01.out
 #SBATCH --error=gen_cc12m_01.err
 #SBATCH --account=EUHPC_R04_192
-#SBATCH --mem=128G
+#SBATCH --mem=256G
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK  
 export NCCL_DEBUG=WARN
