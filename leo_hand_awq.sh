e62f65b763401d2cf4b3eecd587ae094e4bb88fa
fesvhtr <fesvhtr@gmail.com>
2025-09-03 19:38:29 +0400

    update

--- leo_hand_awq.sh
+++ leo_hand_awq.sh
@@ -2,15 +2,15 @@
 #SBATCH --job-name=gen_hand_visual
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:2
 #SBATCH --partition=boost_usr_prod
 #SBATCH --qos=normal
-#SBATCH --output=gen_hand_visual_fulldata_4gpu.out
-#SBATCH --error=gen_hand_visual_fulldata_4gpu.err
+#SBATCH --output=gen_hand_visual_fulldata.out
+#SBATCH --error=gen_hand_visual_fulldata.err
 #SBATCH --account=EUHPC_R04_192
-#SBATCH --mem=256G
+#SBATCH --mem=128G
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK  
 export NCCL_DEBUG=WARN
@@ -26,19 +26,19 @@ cd $WORK/fmohamma/CLIP-R/
 
 # run python
 python -u dataset/gen_vllm_ray_visual.py \
-    --model_source $WORK/fmohamma/CLIP-R/data/Qwen2.5-VL-72B-Instruct \
-    --output_dir_path  $WORK/fmohamma/CLIP-R/outputs/Hand-ICL/fulldata \
+    --model_source $WORK/fmohamma/CLIP-R/data/Qwen2.5-VL-72B-Instruct-AWQ \
+    --output_dir_path  $WORK/fmohamma/CLIP-R/outputs/Hand-ICL/fulldata-awq \
     --image_dir_path $WORK/fmohamma/CLIP-R/data/Nicous-Hand-ICL/fulldata \
-    --checkpoint_interval 100000 \
-    --ray_batch_size 10000 \
-    --batch_size 64 \
+    --checkpoint_interval 1000 \
+    --ray_batch_size 1000 \
+    --batch_size 16 \
     --max_model_len 2048 \
-    --max_num_batched_tokens 98304 \
-    --max_num_seqs 64 \
+    --max_num_batched_tokens 32768 \
+    --max_num_seqs 16 \
     --max_tokens 1024 \
     --temperature 0.8 \
     --top_p 0.95 \
-    --tensor_parallel_size 4 \
+    --tensor_parallel_size 2 \
     --pipeline_parallel_size 1 \
     --gpu_memory_utilization 0.9 \
     --enable_chunked_prefill \
@@ -47,5 +47,6 @@ python -u dataset/gen_vllm_ray_visual.py \
     --concurrency 1 \
     --num_workers 8 \
     --log_level INFO \
-    --dtype auto \
+    --dtype float16 \
+    --quantization awq \
     --enable_resume
