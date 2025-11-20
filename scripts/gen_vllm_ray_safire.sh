411b632e383f3062ab099a37aff4ab0f245aab07
fesvhtr <fesvhtr@gmail.com>
2025-11-20 18:48:23 +0400

    add intervl gen

--- scripts/gen_vllm_ray_safire.sh
+++ scripts/gen_vllm_ray_safire.sh
@@ -2,9 +2,9 @@
 
 export CUDA_VISIBLE_DEVICES=0,1
 python -u dataset/gen_vllm_ray_visual.py \
-    --model_source /leonardo_scratch/fast/EUHPC_R04_192/fmohamma/fast_weights/Qwen3-VL-8B-Instruct \
+    --model_source /leonardo_scratch/fast/EUHPC_R04_192/fmohamma/fast_weights/InternVL3_5-8B-HF \
     --parquet_dir_path $WORK/fmohamma/CLIP-R/data/UniFire_11K/mcqa \
-    --output_dir_path  $WORK/fmohamma/CLIP-R/outputs/Safire/Qwen3-VL-8B-Instruct \
+    --output_dir_path  $WORK/fmohamma/CLIP-R/outputs/Safire/InternVL3_5-8B \
     --checkpoint_interval 30000 \
     --ray_batch_size 3000 \
     --batch_size 24 \
