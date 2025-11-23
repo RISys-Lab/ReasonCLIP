19bafe1fadcb946a81c238ef6a9dd7339ce83ad1
fesvhtr <fesvhtr@gmail.com>
2025-11-23 14:53:38 +0400

    upd

--- scripts/leo_safire.sh
+++ scripts/leo_safire.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=gen_safire_qwen3-vl-8b-instruct
+#SBATCH --job-name=gen_safire_qwen3-vl-32b-instruct
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:4
 #SBATCH --partition=boost_usr_prod
 #SBATCH --qos=normal
-#SBATCH --output=gen_safire_qwen3-vl-8b-instruct.out
-#SBATCH --error=gen_safire_qwen3-vl-8b-instruct.err
+#SBATCH --output=gen_safire_qwen3-vl-32b-instruct.out
+#SBATCH --error=gen_safire_qwen3-vl-32b-instruct.err
 #SBATCH --account=EUHPC_R04_192
 #SBATCH --mem=256G
 
@@ -28,21 +28,21 @@ cd $WORK/fmohamma/CLIP-R/
 
 # run python
 python -u dataset/gen_vllm_ray_visual.py \
-    --model_source /leonardo_work/EUHPC_R04_192/fmohamma/zsc/LLaMA-Factory-private/data/models/Safire-Qwen3-VL-8B-Instruct \
+    --model_source /leonardo_scratch/fast/EUHPC_R04_192/fmohamma/fast_weights/Safire-Qwen3-VL-32B-Instruct \
     --parquet_dir_path $WORK/fmohamma/CLIP-R/data/UniFire_11K/mcqa \
-    --output_dir_path  $WORK/fmohamma/CLIP-R/outputs/Safire/Safire-Qwen3-VL-8B-Instruct \
+    --output_dir_path  $WORK/fmohamma/CLIP-R/outputs/Safire/Safire-Qwen3-VL-32B-Instruct \
     --checkpoint_interval 50000 \
     --ray_batch_size 2000 \
-    --batch_size 16 \
+    --batch_size 24 \
     --max_model_len 2048 \
     --max_num_batched_tokens 32768 \
-    --max_num_seqs 16 \
+    --max_num_seqs 24 \
     --max_tokens 2048\
     --temperature 0.0 \
     --top_p 1.0 \
-    --tensor_parallel_size 2 \
+    --tensor_parallel_size 4 \
     --pipeline_parallel_size 1 \
-    --gpu_memory_utilization 0.9 \
+    --gpu_memory_utilization 0.95 \
     --enable_chunked_prefill \
     --trust_remote_code \
     --mm_processor_cache_gb 0 \
