d16a1e0809ac39981a38d1ce18eb32fc36e6ffda
fesvhtr <fesvhtr@gmail.com>
2026-02-09 14:39:19 +0400

    upd

--- scripts/eval_retrieval.sh
+++ scripts/eval_retrieval.sh
@@ -54,24 +54,24 @@ for i in "${!models[@]}"; do
 done
 
 
-for i in "${!models[@]}"; do
-  python eval/eval_retrieval.py \
-    --model_path "${models[$i]}" \
-    --processor_path "${processors[$i]}" \
-    --model_name auto \
-    --dataset_name mscoco \
-    --split test \
-    --coco_captions_json "$COCO2017_CAPTIONS_JSON" \
-    --local_image_dir "$COCO2017_IMAGE_DIR" \
-    --batch_size 512 \
-    --device cuda:0 \
-    --skip_if_exists \
-    --results_dir "$WORK/fmohamma/CLIP-R/eval/results/retrieval_coco2017" &
+# for i in "${!models[@]}"; do
+#   python eval/eval_retrieval.py \
+#     --model_path "${models[$i]}" \
+#     --processor_path "${processors[$i]}" \
+#     --model_name auto \
+#     --dataset_name mscoco \
+#     --split test \
+#     --coco_captions_json "$COCO2017_CAPTIONS_JSON" \
+#     --local_image_dir "$COCO2017_IMAGE_DIR" \
+#     --batch_size 512 \
+#     --device cuda:0 \
+#     --skip_if_exists \
+#     --results_dir "$WORK/fmohamma/CLIP-R/eval/results/retrieval_coco2017" &
 
-  while [ "$(jobs -rp | wc -l)" -ge 1 ]; do
-    wait -n
-  done
-done
+#   while [ "$(jobs -rp | wc -l)" -ge 1 ]; do
+#     wait -n
+#   done
+# done
 
 for i in "${!models[@]}"; do
   python eval/eval_retrieval.py \
