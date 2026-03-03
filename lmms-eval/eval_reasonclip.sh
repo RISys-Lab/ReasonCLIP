8f229a6159fa3b415055b9cdf0d4af86c6acfd56
fesvhtr <fesvhtr@gmail.com>
2026-03-03 21:14:42 +0000

    upd

--- lmms-eval/eval_reasonclip.sh
+++ lmms-eval/eval_reasonclip.sh
@@ -8,16 +8,16 @@ cd /home/localadmin/bz/CLIP-R/lmms-eval
 python -m lmms_eval \
   --model llava_clipr \
   --model_args pretrained=/home/localadmin/bz/CLIP-R/llava_next/checkpoints/merged/clipr_qwen3_sft,model_name=qwen3,vision_tower_name=fesvhtr/clip-r-336-s1-run1215-1280,conv_template=qwen_1_5,device_map=auto \
-  --tasks mmbench_en \
+  --tasks hallusion_bench_image \
   --batch_size 1 \
   --log_samples \
-  --output_path ./outputs/mmbench_en
+  --output_path ./outputs/hallusion_bench_image
 
 # 2) CLIP vision tower version
 python -m lmms_eval \
   --model llava_clipr \
   --model_args pretrained=/home/localadmin/bz/CLIP-R/llava_next/checkpoints/merged/clip_qwen3_sft,model_name=qwen3,vision_tower_name=openai/clip-vit-large-patch14-336,conv_template=qwen_1_5,device_map=auto \
-  --tasks mmbench_en  \
+  --tasks hallusion_bench_image \
   --batch_size 1 \
   --log_samples \
-  --output_path ./outputs/mmbench_en
+  --output_path ./outputs/hallusion_bench_image
